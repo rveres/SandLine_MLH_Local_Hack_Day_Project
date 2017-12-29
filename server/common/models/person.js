2bd658aa3983c541c7176df4405c6659c14faf9d
Robert Veres <thedeveloper733@gmail.com>
2017-12-29 18:44:06 -0500

    Disabiling unnecessary HTTP Methods

--- server/common/models/person.js
+++ server/common/models/person.js
@@ -2,4 +2,11 @@
 
 module.exports = function(Person) {
 
+    Person.disableRemoteMethodByName("create", true);
+    Person.disableRemoteMethod("upsert", true);
+    Person.disableRemoteMethod("updateAll", true);
+    Person.disableRemoteMethod("updateAttributes", false);
+
+    Person.disableRemoteMethod("findById", true);
+
 };
