fac4ae04b6c5e4b3015df2bac273b0029fd7d2f1
Robert Veres <thedeveloper733@gmail.com>
2017-12-30 11:01:05 -0500

    Adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainFlowComponent } from './main-flow/main-flow.component';
+import { LoginFlowComponent } from './login-flow/login-flow.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginFlowComponent
+  },
+  {
+    path: 'order',
+    component: MainFlowComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
